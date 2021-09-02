aba5684ff7475a6878714025f2a6aa69e6d3f8ff
adulalhy <46670590+adulalhy@users.noreply.github.com>
2021-09-02 07:08:28 +0700

    Add files via upload

---
+++ commado/help.js
@@ -0,0 +1,38 @@
+const help = (f) => {
+	return `┏━━⬣ 𝙎𝙀𝙇𝙁 𝘽𝙊𝙏
+┃++[OWNER]++
+┃> [eval]
+┃${f}term [code]
+┃${f}upswteks
+┃${f}upswvideo
+┃${f}upswimage
+┃${f}mode [button]
+┃${f}setprefix [buttton]
+┃${f}setfakeimg [reply]
+┃${f}setthumb [reply]
+┃${f}bc
+┃${f}clearall
+┃${f}setpp
+┃${f}leave
+┃
+┃++[OTHER]++
+┃${f}tomp4
+┃${f}sherelock
+┃${f}detikvideo
+┃${f}detikvn
+┃${f}kick
+┃${f}add
+┃${f}runtime
+┃${f}ping
+┃${f}group [button]
+┃${f}listadmin
+┃${f}tagall
+┃${f}hidetag
+┃${f}stiker
+┃${f}toimg
+┃${f}play
+┃${f}video
+┗⬣`
+}
+
+exports.help = help
