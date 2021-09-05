19145f2c5f0c914c3f236dea97c08cc9fe532eed
Seazyc <83223470+Seazyc@users.noreply.github.com>
2021-09-05 18:01:56 +0700

    Update help.js

--- commado/help.js
+++ commado/help.js
@@ -3,18 +3,25 @@ const help = (f) => {
 ┏━━⬣ 𝙊𝙒𝙉𝙀𝙍
 ┃
 ┃> [eval]
+┃=> [eval async]
+┃$ [code]
 ┃${f}term [code]
 ┃${f}upswteks
 ┃${f}upswvideo
 ┃${f}upswimage
 ┃${f}mode [button]
 ┃${f}setprefix [buttton]
+┃${f}antiviewonce[button]
 ┃${f}setfakeimg [reply]
 ┃${f}setthumb [reply]
 ┃${f}bc
 ┃${f}clearall
 ┃${f}setpp
 ┃${f}leave
+┃${f}q
+┃${f}get
+┃${f}sider
+┃${f}setnama
 ┗⬣
 
 ┏━━⬣ 𝙊𝙏𝙃𝙀𝙍
@@ -35,7 +42,43 @@ const help = (f) => {
 ┃${f}toimg
 ┃${f}play
 ┃${f}video
-┗⬣`
+┃${f}inspect
+┃${f}pinterest
+┃${f}playstore
+┃${f}take
+┃${f}getpic
+┃${f}linkwa
+┃${f}translate
+┃${f}rulesgroup
+┃${f}ig story
+┃${f}lirik
+┗⬣
+
+┏━━⬣ 𝙎𝙏𝘾 𝘾𝙈𝘿
+┃
+┃${f}addcmd
+┃${f}delcmd
+┃${f}listcmd
+┗⬣
+
+┏━━⬣ 𝙁𝙐𝙉
+┃
+┃${f}readmore
+┃${f}jadian
+┃${f}ganteng
+┃${f}beban
+┃${f}cantik
+┃${f}jelek
+┃${f}nganggur
+┃${f}hebat
+┃${f}wibu
+┃${f}pakboy
+┃${f}pakgirl
+┃${f}sange
+┃${f}nolep
+┃${f}jahat
+┃${f}baik
+┗⬣
 }
 
 exports.help = help
